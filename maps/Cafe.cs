93928fd6ba9789254654518ded7bf00de38d1c2b
armen <armen@ARTMAN-N56VB>
2017-04-28 11:19:01 +0400

    Add project files.

---
+++ maps/Cafe.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maps
+{
+    class Cafe : Building
+    {
+        private string address;
+        private string website;
+        private string number;
+        private DateTime opening;
+        private DateTime closing;
+        public Cafe(string address, string website, string number, DateTime opening, DateTime closing)
+            : base(address, website, number, opening, closing)
+        {
+            this.address = address;
+            this.website = website;
+            this.number = number;
+            this.opening = opening;
+            this.closing = closing;
+        }
+
+        public override string ToString()
+        {
+            return "This is a cafe";
+        }
+        public string WorkingHours()
+        {
+            return this.opening.Hour + ":" + this.opening.Minute + " - " + this.closing.Hour + ":" + this.closing.Minute;
+        }
+    }
+}
---
+++ maps/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cafe cafe = new Cafe("a", "b", "c", DateTime.Parse("10:30:00"), DateTime.Parse("19:30:00"));
+            cafe.AddRating(5);
+            cafe.AddRating(4);
+            cafe.AddRating(5);
+            cafe.AddRating(3);
+            cafe.AddVisitors();
+            cafe.AddVisitors();
+            cafe.AddRating(5);
+            cafe.AddVisitors();
+            Console.WriteLine(cafe.Rating);
+            Console.WriteLine(cafe.Popularity);
+            Console.WriteLine(cafe.ToString());
+            Console.WriteLine("This cafe works: " + cafe.WorkingHours());
+        }
+    }
+}
