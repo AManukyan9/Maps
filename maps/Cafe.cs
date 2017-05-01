01b20e91000263ddefdff398a12e7710d7b293ed
AregVrt <AregVrt@users.noreply.github.com>
2017-05-01 16:32:09 +0400

    Add files via upload
    Added new functions.

--- maps/Cafe.cs
+++ maps/Cafe.cs
@@ -3,40 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Device;
 
 
 namespace maps
 {
-    class Cafe : Building
-    {
-        private string address;
-        private string website;
-        private string number;
-        private DateTime opening;
-        private DateTime closing;
-        private static int count;
-        public Cafe(string address, string website, string number, DateTime opening, DateTime closing)
-            : base(address, website, number, opening, closing)
-        {
-            this.address = address;
-            this.website = website;
-            this.number = number;
-            this.opening = opening;
-            this.closing = closing;
-            count++;
-        }
+	class Cafe : Building
+	{
+		private string name;
+		private string address;
+		private string website;
+		private string number;
+		private DateTime opening;
+		private DateTime closing;
+		private static int count;
+		public Cafe(string address, string website, string number, DateTime opening, DateTime closing)
+			: base(address, website, number, opening, closing)
+		{
+			this.address = address;
+			this.website = website;
+			this.number = number;
+			this.opening = opening;
+			this.closing = closing;
+			count++;
+		}
 
-        public override string ToString()
-        {
-            return "This is a cafe";
-        }
+		public override string ToString()
+		{
+			if(this.name == ""){
+				return "Cafe";
+			} else {
+				return "Cafe " + this.name;
+			}
+		}
 
-        public string WorkingHours()
-        {
-            return this.opening.Hour + ":" + this.opening.Minute + " - " + this.closing.Hour + ":" + this.closing.Minute;
-        }
+		public string WorkingHours()
+		{
+			return this.opening.Hour + ":" + this.opening.Minute + " - " + this.closing.Hour + ":" + this.closing.Minute;
+		}
 
-        
-    }
+
+	}
 }
