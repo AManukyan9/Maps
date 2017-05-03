dac179df8b0575263810ea32508778e5a0c352a8
armen <armman999@gmail.com>
2017-05-03 12:04:58 +0400

    added stuff

--- maps/Program.cs
+++ maps/Program.cs
@@ -15,10 +15,14 @@ namespace maps
             Maps map = new Maps();
             Point addressCoord = new Point(13, 10);
             Address ad = new Address("address", addressCoord);
+            Point addressCoord1 = new Point(73, 13);
+            Address ad1 = new Address("address", addressCoord1);
+            Point addressCoord2 = new Point(34, 2);
+            Address ad2 = new Address("address", addressCoord2);
             Cafe cafe = new Cafe("Restoranchik", ad, "b", "c", DateTime.Parse("10:30:00"), DateTime.Parse("19:30:00"));
-            Cafe cafe1 = new Cafe("Restoranchik1", ad, "b", "c", DateTime.Parse("10:30:00"), DateTime.Parse("19:30:00"));
-            Cafe cafe2 = new Cafe("Restoranchik2", ad, "b", "c", DateTime.Parse("10:30:00"), DateTime.Parse("19:30:00"));
-            Console.WriteLine(map.FindCafe("Restoranchik"));
+            Cafe cafe1 = new Cafe("Restoranchik1", ad1, "b", "c", DateTime.Parse("10:30:00"), DateTime.Parse("19:30:00"));
+            Cafe cafe2 = new Cafe("Restoranchik2", ad2, "b", "c", DateTime.Parse("10:30:00"), DateTime.Parse("19:30:00"));
+            Console.WriteLine(map.FindCafe("Restoranchik2"));
                       
         }
     }
