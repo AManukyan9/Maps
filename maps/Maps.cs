37d53527055ba77f9b51e3068ec22528fac425c8
VaheHayrapetyan <hayrapetyanv38@gmail.com>
2017-05-05 21:53:11 +0400

    Add files via upload

--- maps/Maps.cs
+++ maps/Maps.cs
@@ -9,40 +9,91 @@ using System.Drawing;
 
 namespace maps
 {
-    class Maps 
+    static class Maps 
     {
         static ArrayList buildings = new ArrayList();
-        static List<Cafe> cafes = new List<Cafe>();
 
         public static double BuildingsDistance(Address a, Address b)
         {
             return Math.Sqrt((a.Cord.X - b.Cord.X) * (a.Cord.X - b.Cord.X) + (a.Cord.Y - b.Cord.Y) * (a.Cord.Y - b.Cord.Y));
         }
 
-        public static ArrayList Nearby(Address address, decimal radius)
+        public static ArrayList Nearby(Address address, double radius)
         {
-            throw new NotImplementedException();
+            ArrayList returnBuildings = new ArrayList();
+            for (int i = 0; i < buildings.Count; i++)
+            {    
+                if (BuildingsDistance(address, ((Building)buildings[i]).Address) <= radius)
+                {
+                    returnBuildings.Add(buildings[i]);
+                }
+            }
+            return returnBuildings;
         }
 
-        public static ArrayList Search(string name = "", decimal radius = 0, decimal rating = 0)
+        public static ArrayList Search(Address address = null, string name = "", decimal rating = 0)
         {
-            throw new NotImplementedException();
+            ArrayList returnBuildings = new ArrayList();
+            bool[] filled = new bool[buildings.Count];
+            if (address == null)
+            {
+                if (name != "")
+                {
+                    for (int i = 0; i < buildings.Count; i++)
+                    {
+                        if ((((Building)buildings[i]).Name == name && ((Building)buildings[i]).Rating >= rating) && filled[i] != true)
+                        {
+                            returnBuildings.Add(buildings[i]);
+                            filled[i] = true;
+                        }
+                    }
+                }
+                for (int i = 0; i < buildings.Count; i++)
+                {
+                    if ((((Building)buildings[i]).Name == name || ((Building)buildings[i]).Rating >= rating) && filled[i] != true)
+                    {
+                        returnBuildings.Add(buildings[i]);
+                        filled[i] = true;
+                    }
+                }
+            }
+            else
+            {
+                if (name != "")
+                {
+                    for (int i = 0; i < buildings.Count; i++)
+                    {
+                        if ((((Building)buildings[i]).Address == address && ((Building)buildings[i]).Name == name && ((Building)buildings[i]).Rating >= rating) && filled[i] != true)
+                        {
+                            returnBuildings.Add(buildings[i]);
+                            filled[i] = true;
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < buildings.Count; i++)
+                    {
+                        if ((((Building)buildings[i]).Address == address && ((Building)buildings[i]).Rating >= rating) && filled[i] != true)
+                        {
+                            returnBuildings.Add(buildings[i]);
+                            filled[i] = true;
+                        }
+                    }
+                }
+                for (int i = 0; i < buildings.Count; i++)
+                {
+                    if ((((Building)buildings[i]).Address == address || ((Building)buildings[i]).Name == name ||((Building)buildings[i]).Rating >= rating) && filled[i] != true)
+                    {
+                        returnBuildings.Add(buildings[i]);
+                        filled[i] = true;
+                    }
+                }
+
+            }     
+            return returnBuildings;   
         }
 
-        public static ArrayList Search(Address address, string name = "", decimal radius = 0, decimal rating = 0)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void AddCafe(Cafe cafe)
-        {
-            cafes.Add(cafe);
-        }
-
-        public static void AddBuilding(Object bld)
-        {
-            buildings.Add(bld);
-        }
     }
 
 }
